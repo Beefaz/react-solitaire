9e157f0f6c63d2c4d5fbf85a9f9fa13b3f6d088a
justinas.geryba@gmail.com <justinas.geryba@gmail.com>
2020-12-12 19:41:21 +0200

    removed clickable states reworked objects to arrays tested deck on array containers seems to fill up ok.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,67 @@
 import React, {useState} from "react";
-import Card from "./components/Card";
 import CardContainer from "./components/CardContainer";
 
 
 const App = () => {
-    const cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    const deckSuits = {
-        hearts: {
-            color: 'red',
-            values: [...cardValues],
-        },
-        diamonds: {
-            color: 'red',
-            values: [...cardValues],
-        },
-        spades: {
-            color: 'black',
-            values: [...cardValues],
-        },
-        clubs: {
-            color: 'black',
-            values: [...cardValues],
-        },
-    };
+        const cardArrays = {
+            newCards: {
+                closedCards: [0],
+                visibleCards: [0],
+            },
+            finishedCards: {
+                hearts: [0],
+                diamonds: [0],
+                spades: [0],
+                clubs: [0],
+            },
+            playableCards: {
+                col1: [0],
+                col2: [0],
+                col3: [0],
+                col4: [0],
+                col5: [0],
+                col6: [0],
+            },
+        };
 
-    const [deckState, setDeckState] = useState(deckSuits);
-    const [cardPickedup, setCardPickedUp] = useState({suit: null, value: null});
+        const cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+        const setcard = (value, color, suit, visibility) => {
+            const card = {};
+            card.value = value;
+            card.color = color;
+            card.suit = suit;
+            card.visibility = visibility;
+            return card
+        };
+
+        const deck = cardValues.map(value => setcard(value, 'red', 'hearts', true))
+            .concat(cardValues.map(value => setcard(value, 'red', 'diamonds', true)))
+            .concat(cardValues.map(value => setcard(value, 'black', 'spades', true)))
+            .concat(cardValues.map(value => setcard(value, 'black', 'clubs', true)));
+
+        const [gamestate, setGamestate] = useState({...cardArrays});
+
         return <div className="App">
             <div style={{display: 'flex', justifyContent: 'space-between'}}>
                 <div style={{display: 'flex'}}>
-                    <Card backgroundColor={'blue'}/>
-                    <CardContainer backgroundColor={'green'}/>
+                    <CardContainer backgroundColor={'green'} cards={gamestate.newCards.closedCards}/>
+                    <CardContainer backgroundColor={'green'} cards={gamestate.newCards.visibleCards}/>
                 </div>
-                <div style={{display: 'flex'}}>
-                    {Object.keys(deckSuits).map((deckSuit) => (
-                        <CardContainer key={deckSuit}
-                                       containerFor={deckSuit}
-                                       deckState={deckState}
-                                       setDeckState={setDeckState}
-                                       cardPickedUp={cardPickedup}/>
-                    ),)}
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                    <CardContainer containerFor={'hearts'} cards={gamestate.finishedCards.hearts}/>
+                    <CardContainer containerFor={'diamonds'} cards={gamestate.finishedCards.diamonds}/>
+                    <CardContainer containerFor={'spades'} cards={gamestate.finishedCards.spades}/>
+                    <CardContainer containerFor={'clubs'} cards={gamestate.finishedCards.clubs}/>
                 </div>
             </div>
-            {Object.keys(deckSuits).map((deckSuit) => (
-                <div style={{display: 'flex'}}>{deckSuits[deckSuit].values.map((cardValue, cardIndex) => (
-                    <Card key={deckSuit.concat(cardValue).concat(cardIndex.toString())}
-                          color={deckSuits[deckSuit].color}
-                          suit={deckSuit}
-                          value={cardValue}
-                          cardPickedUp={cardPickedup}
-                          setCardPickedUp={setCardPickedUp}/>
-                ))}
-                </div>
-            ),)}
+            <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                {Object.keys(cardArrays.playableCards).map((container) => (
+                    <CardContainer key={container} cards={gamestate.playableCards[container]}/>
+                ),)}
+            </div>
         </div>
-};
+    }
+;
 
 export default App;
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,41 +1,23 @@
 import React, {useState} from "react";
+import Card from "./Card";
 
 const CardContainer = (props) => {
-    const cardStack = [{suit: props.suit, value: null}];
-    const [cardsInContainer, setCardsInContainer] = useState(cardStack);
-    const [bordercolor, setBorderColor] = useState(false);
 
-    const cardStyle = {
+    const container = {
         height: '150px',
         width: '100px',
         padding: '5px',
         margin: '5px',
         borderStyle: 'solid',
-        borderWidth: bordercolor ? '5px' : '2px',
-        borderColor: bordercolor ? 'red' : 'black',
         borderRadius: '10px',
         backgroundColor: 'green' && props.backgroundColor,
     };
-    const cardLabelStyle = {
-        color: props.color,
-        position: 'relative',
-        left: '0',
-        top: '0'
-    };
-    const clickHandler = () => {
-        const currentStack = [...cardsInContainer].pop();
-        if (currentStack.value === null
-            && currentStack + 1 !== props.cardPickedUp.value
-            && currentStack.suit !== props.cardPickedUp.suit) {
-            setBorderColor(!bordercolor);
-        } else {
-            setCardsInContainer(cardsInContainer.push(props.cardPickedUp))
-        }
-    };
-    return <div style={cardStyle}
-                onClick={clickHandler}>
-        {console.table(cardsInContainer)}
-        <div style={cardLabelStyle}>{props.containerFor}</div>
+
+    return <div style={container}>
+        {props.cards.map((card) => {
+            if (props.cards.length > 1) {return <Card key={card} color={card.color} visibility={card.visibility} suit={card.suit} value={card.value}/>}
+            else return null;
+        },)}
     </div>;
 };
 export default CardContainer;
